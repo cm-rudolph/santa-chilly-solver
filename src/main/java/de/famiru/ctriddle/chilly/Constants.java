28991095e25a6319538a885491a4c52cabfeed6d
Christian Rudolph <6566795+cm-rudolph@users.noreply.github.com>
2023-12-11 19:15:39 +0100

    missing final

--- src/main/java/de/famiru/ctriddle/chilly/Constants.java
+++ src/main/java/de/famiru/ctriddle/chilly/Constants.java
@@ -2,8 +2,8 @@ package de.famiru.ctriddle.chilly;
 
 public final class Constants {
     public static final String EXIT_TO_START_PATH = "exit to start";
-    public static int ATSP_TO_STSP_WEIGHT = 200;
-    public static int INFINITY = 99999999 - ATSP_TO_STSP_WEIGHT;
+    public static final int ATSP_TO_STSP_WEIGHT = 200;
+    public static final int INFINITY = 99999999 - ATSP_TO_STSP_WEIGHT;
 
     private Constants() {
     }
