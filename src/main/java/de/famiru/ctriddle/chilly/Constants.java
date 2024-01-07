7d5a5a9867a96e93ec85961a83e5e8d1bd64b4a9
Christian Rudolph <6566795+cm-rudolph@users.noreply.github.com>
2024-01-07 12:02:43 +0100

    increase weight to make cluster connections cheaper

--- src/main/java/de/famiru/ctriddle/chilly/Constants.java
+++ src/main/java/de/famiru/ctriddle/chilly/Constants.java
@@ -2,7 +2,7 @@ package de.famiru.ctriddle.chilly;
 
 public final class Constants {
     public static final String EXIT_TO_START_PATH = "exit to start";
-    public static final int ATSP_TO_STSP_WEIGHT = 200;
+    public static final int ATSP_TO_STSP_WEIGHT = 2000;
     public static final int INFINITY = 99999999 - ATSP_TO_STSP_WEIGHT;
 
     private Constants() {
